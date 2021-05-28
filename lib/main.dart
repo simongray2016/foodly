90f9eb8e3117eb39feceb02de8a1e4d6f39b95ac
Daniel Trương <simongray2016@gmail.com>
2021-05-29 00:38:12 +0700

    Innit Firebase for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'constants.dart';
 import 'routes.dart';
 import 'screens/splash/splash_screen.dart';
 import 'theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(Foodly());
 }
 
-class Foodly extends StatelessWidget {
+class Foodly extends StatefulWidget {
+  @override
+  _FoodlyState createState() => _FoodlyState();
+}
+
+class _FoodlyState extends State<Foodly> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        // return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return FirebaseLoading();
+      },
+    );
+  }
+}
+
+class FirebaseLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Saving money',
+      home: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Center(child: Text('loading')),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
